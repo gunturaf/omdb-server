aba19cbb8a7957597d42bb898a11bf31e70383c6
Guntur Akhmad <guntur.akhmad@styletheory.co>
2020-11-19 11:16:55 +0700

    set mysql db dsn to config

--- config/consts.go
+++ config/consts.go
@@ -5,4 +5,10 @@ const (
 	OMDBApiKey      = "OMDB_API_KEY"
 	HTTPServicePort = "HTTP_SERVICE_PORT"
 	GRPCServicePort = "GRPC_SERVICE_PORT"
+
+	MysqlUsername = "MYSQL_USERNAME"
+	MysqlPassword = "MYSQL_PASSWORD"
+	MysqlHost     = "MYSQL_HOST"
+	MysqlPort     = "MYSQL_PORT"
+	MysqlDBName   = "MYSQL_DBNAME"
 )
